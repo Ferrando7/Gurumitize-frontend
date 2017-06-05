3f113ed375d20097776c76b2da03af609a591cba
Ferrando7 <oscarfesan@hotmail.com>
2017-06-06 01:55:54 +0200

    viewEvent<...>.name added

--- src/app.js
+++ src/app.js
@@ -9,6 +9,7 @@ import 'angular-material/angular-material.css';
 import ngMdIcons from 'angular-material-icons';
 
 import MoviesService from './services/movies/movies';
+import EventsService from './services/events/events';
 import UserService from './services/user/user';
 
 import Routes from './config/routes';
@@ -19,6 +20,10 @@ import ViewMovies from './components/view-movies/view-movies';
 import ViewMovie from './components/view-movie/view-movie';
 import ViewMovieEdit from './components/view-movie-edit/view-movie-edit';
 import ViewMovieCreate from './components/view-movie-create/view-movie-create';
+import ViewEvents from './components/view-events/view-events';
+import ViewEvent from './components/view-event/view-event';
+import ViewEventEdit from './components/view-event-edit/view-event-edit';
+import ViewEventCreate from './components/view-event-create/view-event-create';
 import ViewLogin from './components/view-login/view-login';
 
 let app = angular.module('app', [
@@ -32,6 +37,11 @@ let app = angular.module('app', [
     ViewMovie.name,
     ViewMovieEdit.name,
     ViewMovieCreate.name,
+    ViewEvents.name,
+    ViewEvent.name,
+    ViewEventEdit.name,
+    ViewEventCreate.name,
+
     ViewLogin.name
 ]);
 
