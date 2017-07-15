6099ace1f87e6d72b5dab54247aacccf79b732ae
Bilal1994 <bilal.ahmed@tum.de>
2017-07-16 01:52:11 +0200

    Did some UI changes. Added place holder files for mentor/mentee feature

---
+++ src/services/friends/friends.service.js
@@ -0,0 +1,80 @@
+'use strict';
+
+
+export default class FriendsService {
+
+    static get $inject(){
+        return ['$http', 'API_URL'];
+    }
+
+    constructor($http,API_URL) {
+        this.$http = $http;
+        this.resourceUrl = `${ API_URL }/Friends/`;
+
+    }
+
+    static get name(){
+        return 'FriendsService';
+    }
+
+    list() {
+
+        let url = this.resourceUrl;
+        return this.$http.get(url).then(responce => {
+
+            return new Promise((resolve, reject) => {
+                resolve(responce.data);
+
+            });
+
+        });
+
+    }
+
+    get(id) {
+        let url = `${ this.resourceUrl }${ id }`;
+        return this.$http.get(url).then(responce => {
+
+            return new Promise((resolve, reject) => {
+                resolve(responce.data);
+            });
+
+        })
+    }
+
+
+    create(Friends) {
+        let url = this.resourceUrl;
+        return this.$http.post(url,Friends).then(responce => {
+
+            return new Promise((resolve, reject) => {
+                resolve(responce.data);
+            });
+
+        })
+    }
+
+    delete(id) {
+        let url = `${ this.resourceUrl }${ id }`;
+        return this.$http.delete(url).then(responce => {
+
+            return new Promise((resolve, reject) => {
+                resolve(responce.status);
+            });
+
+        })
+    }
+
+    update(Friends) {
+
+        let url = `${ this.resourceUrl }${ Friends['_id'] }`;
+        return this.$http.put(url,Friends).then(responce => {
+
+            return new Promise((resolve, reject) => {
+                resolve(responce.data);
+            });
+
+        })
+    }
+
+}
